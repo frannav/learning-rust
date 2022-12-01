76c49a9cb4d7371b1372e1493db1ad71a863c984
frannav <frannavmart@gmail.com>
2022-12-01 20:22:55 +0000

    added first practise solution v1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("_ _ _ _ _ _ _ _ _ _ ");
+    println!("- Guess the number!");
+    println!("- Input your guess");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=4);
+
+    let mut guess: String = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed reading input");
+
+    let guess: u32 = guess.trim().parse().expect("Type a number please!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low!"),
+        Ordering::Equal => println!("You win! 🏆"),
+        Ordering::Greater => println!("Too high!"),
+    }
+
+    println!("- You guessed: {guess}");
+    println!("_ _ _ _ _ _ _ _ _ _ ");
 }
